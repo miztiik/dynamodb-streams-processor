cd80c7d29ec1d4167fa24b870213a48b9d2fdfef
Mystique <13oct08@quantumfoam.uni.cc>
2019-05-07 22:40:04 +0200

    Updated for stream processor in Python

--- helper_scripts/deploy.sh
+++ helper_scripts/deploy.sh
@@ -4,16 +4,20 @@ set -ex
 #----- Change these parameters to suit your environment -----#
 AWS_PROFILE="default"
 BUCKET_NAME="sam-templates-011" # bucket must exist in the SAME region the deployment is taking place
-TEMPLATE_NAME="dynamodb-stream-processor.yaml"
-STACK_NAME="dynamodb-stream-processor"
+SERVICE_NAME="dynamodb-stream-processor"
+TEMPLATE_NAME="${SERVICE_NAME}.yaml"
+STACK_NAME="${SERVICE_NAME}"
 OUTPUT_DIR="./outputs/"
 PACKAGED_OUTPUT_TEMPLATE="${OUTPUT_DIR}${STACK_NAME}-packaged-template.yaml"
-SERVICE_NAME="dynamodb-stream-processor"
+
 #----- End of user parameters  -----#
 
 
 # You can also change these parameters but it's not required
-enableDEBUGGER="true"
+# enableDEBUGGER="True"
+
+# Cleanup Output directory
+rm -rf "${OUTPUT_DIR}"*
 
 # Package the code
 aws cloudformation package \
@@ -21,16 +25,13 @@ aws cloudformation package \
     --s3-bucket "${BUCKET_NAME}" \
     --output-template-file "${PACKAGED_OUTPUT_TEMPLATE}"
 
-
-# Cleanup Output directory
-rm -rf ./outpus/*
-
 # Deploy the stack
 aws cloudformation deploy \
     --profile "${AWS_PROFILE}" \
     --template-file "${PACKAGED_OUTPUT_TEMPLATE}" \
     --stack-name "${STACK_NAME}" \
-    --parameter-overrides \
-        enableDEBUGGER="${enableDEBUGGER}" \
     --tags Service="${SERVICE_NAME}" \
     --capabilities CAPABILITY_IAM
+    # --parameter-overrides \
+    #    enableDEBUGGER="${enableDEBUGGER}" \
+
